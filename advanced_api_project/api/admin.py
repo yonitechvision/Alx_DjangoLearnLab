0eb7eaa4146823bd19d9eb2183780f417c0301f4
yonitechvision <desisayonatan77@gmail.com>
2024-09-08 22:54:01 +0300

    update_advanced_api_django_project

---
+++ advanced_api_project/api/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Author, Book
+
+#create here
+
+admin.site.register(Author)
+admin.site.register(Book)
---
+++ advanced_api_project/api/views.py
@@ -0,0 +1,30 @@
+#from django.shortcuts import render
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Book
+from .serializers import BookSerializer
+
+# Create your views here.
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'author__name', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
