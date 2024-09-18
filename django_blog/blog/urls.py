58912c38e072dd6119b713f54b2b3fe379e38094
yonitechvision <162969020+yonitechvision@users.noreply.github.com>
2024-09-18 10:09:42 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(), name='login'),  # Add this line
+    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
 ]
