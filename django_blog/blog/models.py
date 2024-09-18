ef51020131d876157f84a577168f26320eaf8025
yonitechvision <162969020+yonitechvision@users.noreply.github.com>
2024-09-18 10:08:41 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager()  # Add this line to enable tagging
 
     def __str__(self):
         return self.title
