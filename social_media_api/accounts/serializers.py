16539d81081c9025b6a51b68cdcbe02a9752b088
yonitechvision <162969020+yonitechvision@users.noreply.github.com>
2024-09-22 23:55:18 +0300

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,13 +1,32 @@
 from rest_framework import serializers
+from .models import CustomUser
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
-User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
+
+class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['id', 'username', 'password', 'bio', 'profile_picture']
+        model = get_user_model()# customUser
+        fields = ('username','bio', 'profile_picture','followers')
 
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+# This serializer handles user registration ensures password is write only wont be returned in the response
+class RegisterSerializer(serializers.ModelSerializer):
+    password  = serializers.CharField(required=True, write_only=True)
+    class Meta:
+        model = get_user_model()
+        fields = ('username','email','password')
+
+    def create(self,validated_data):
+            user = get_user_model().objects.create_user( # the create_user handles the password hashing
+                username=validated_data['username'],
+                email=validated_data['email'],
+                password=validated_data['password'],
+
+            )
+            Token.objects.create(user=user) # creating a token for the user after registration
+            return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
